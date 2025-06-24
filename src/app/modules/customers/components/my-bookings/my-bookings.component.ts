3ed484b40e0d0c456e0c4f49afa7c2c4f41c794f
Varshini <nerallavarshini@gmail.com>
2025-06-24 17:55:57 +0530

    Use environment.apiUrl in MyBookingsComponent instead of localhost

--- src/app/modules/customers/components/my-bookings/my-bookings.component.ts
+++ src/app/modules/customers/components/my-bookings/my-bookings.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { environment } from '../../../../../environments/environment';
+
 
 @Component({
   selector: 'app-my-bookings',
@@ -20,19 +22,19 @@ export class MyBookingsComponent implements OnInit {
     setTimeout(() => {
       this.showButtons = false;
     }, 600000); 
-    this.http.get<any[]>('http://localhost:3000/bookings').subscribe((data) => {
+    // this.http.get<any[]>('http://localhost:3000/bookings')
+    this.http.get<any[]>(`${environment.apiUrl}/bookings`).subscribe((data) => {
       this.bookings = data.filter(booking => booking.userId === loggedInUserId);
     });
   }
 
   cancelBooking(bookingId: string) {
-
     const bookingIndex = this.bookings.findIndex(b => b.id === bookingId);
     if (bookingIndex !== -1) {
       this.bookings[bookingIndex].status = "Cancelled";
     }
-
-    this.http.patch(`http://localhost:3000/bookings/${bookingId}`, { status: "Cancelled" })
+// `http://localhost:3000/bookings/${bookingId}`
+    this.http.patch(`${environment.apiUrl}/bookings/${bookingId}`, { status: "Cancelled" })
       .subscribe(() => {
         console.log(`Booking ${bookingId} cancelled successfully.`);
       }, error => {
