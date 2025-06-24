ff6af6acbda502b0d8193beef96406d17630685e
Varshini <nerallavarshini@gmail.com>
2025-06-24 12:50:28 +0530

    Update login component to call AuthService.login()

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -14,6 +14,7 @@ import { AuthService } from '../../services/auth/auth.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { HttpClient } from '@angular/common/http';
 import { StorageService } from '../../services/storage/storage.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   standalone: true,
@@ -38,7 +39,8 @@ import { StorageService } from '../../services/storage/storage.service';
 export class LoginComponent {
   isSpinning: boolean = false;
   loginForm!: FormGroup;
-  private apiUrl = 'http://localhost:3000/users';
+  // private apiUrl = 'http://localhost:3000/users';
+   private apiUrl =`${environment.apiUrl}/users`;
   constructor(
     private fb: FormBuilder,
     private message: NzMessageService,
@@ -58,37 +60,6 @@ export class LoginComponent {
     });
   }
 
-  // login() {
-  //   if (this.loginForm.valid) {
-  //     const { email, password } = this.loginForm.value;
-  //     this.isSpinning = true;
-
-  //     this.http.get<any[]>(this.apiUrl).subscribe(users => {
-  //       const user = users.find(u => u.email === email && u.password === password);
-
-  //       if (user) {
-  //         this.message.success('Login successful!');
-  //         this.storageService.saveUserDetails(user);
-
-  //         if (this.storageService.isAdminLoggedIn()) {
-  //           this.router.navigateByUrl("/admin/dashboard")
-  //         } else if (this.storageService.isCustomerLoggedIn()) {
-  //           this.router.navigateByUrl("/customer/dashboard");
-  //         } else {
-  //           this.message.error('Invalid email or password. Please try again.');
-  //         }
-  //       }
-  //       this.isSpinning = false;
-  //     }, error => {
-  //       this.message.error('Error connecting to the server.');
-  //       this.isSpinning = false;
-  //     });
-
-  //   } else {
-  //     this.message.error('Please fill in all required fields correctly.');
-  //   }
-  // }
-
   login() {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
@@ -98,7 +69,7 @@ export class LoginComponent {
         const user = users.find(u => u.email === email && u.password === password);
   
         if (user) {
-          console.log("User found:", user);  // ✅ Add this line for debugging
+          console.log("User found:", user);  
   
           this.message.success('Login successful!');
           this.storageService.saveUserDetails(user);
